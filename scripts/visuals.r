f19aebf05ebd68006bee2fa471e6230bc035d328
Tokazama <zchristensen7@gmail.com>
2016-03-14 16:08:03 -0600

    Update visuals.r

--- scripts/visuals.r
+++ scripts/visuals.r
@@ -23,13 +23,34 @@ imat <- imageListToMatrix(ilist, mask)
 n <- nrow(imat) # number of subjects
 x <- diag(n) # desing matrix that represents each image as a factor
 fit <- .lm.fit(x, imat)
-c <- rep(1, n)
-c[ sub_ucsd[rownames(sub_ucsd) %in% "105"] ] <- -(n-1) # contrast subject image against everyone else
 mrss <- sqrt(fit$residuals / (n - 1)) # mean residual sum of squares
+
+# here we create a visual for a singl subject
+c <- rep(1, n)
+c[82] <- -(n-1) # contrast subject image against everyone else 
+                # 82 correpsonds to the the subject at row 82
+                # in the `sub_ucsd` data frame
 se <- t(as.matrix( mrss * c %*% XX %*% c)) # standard errors
 se[se == 0] <- 1 # 0/0 produces NAs, so now all 0s are divided by 1
 statmat <- (c %*% fit$coefficients) / se # create t-statistic
-
 istat <- makeImage(mask, statmat)
 
 
+# alternatively, we can loop over the entire dataset
+x <- matrix(nrow = n, ncol = 2)
+x[, 1] <- 1
+x[,2] <- 0
+new_ilist <- list()
+c <- matrix(c(1, -1), nrow =1 ) # contrast
+for ( i in 1:n){
+  if (i > 1)
+    x[i - 1, 2] <- 0
+  x[i, 2] <- 1
+  fit <- .lm.fit(x, imat)
+  mrss <- sqrt(fit$residuals / (n - 1))
+  se <- t(as.matrix( mrss * c %*% XX %*% c))
+  se[se == 0] <- 1
+  statmat <- (c %*% fit$coefficients) / se
+  img <- makeImage(mask, statmat)
+  renderSurfaceFunction
+}
