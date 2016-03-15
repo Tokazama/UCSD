97ca71f0a99826e0e8ce58cce25d4b9c07147d89
Tokazama <zchristensen7@gmail.com>
2016-03-15 07:51:44 -0600

    Update visuals.r

--- scripts/visuals.r
+++ scripts/visuals.r
@@ -21,36 +21,48 @@ imat <- imageListToMatrix(ilist, mask)
 
 # we now need to normalize each image by fitting it to an ANOVA model
 n <- nrow(imat) # number of subjects
-x <- diag(n) # desing matrix that represents each image as a factor
-fit <- .lm.fit(x, imat)
-mrss <- sqrt(fit$residuals / (n - 1)) # mean residual sum of squares
+
 
 # here we create a visual for a singl subject
-c <- rep(1, n)
-c[82] <- -(n-1) # contrast subject image against everyone else 
-                # 82 correpsonds to the the subject at row 82
+x <- matrix(nrow = n, ncol = 2)
+x[, 1] <- 1
+x[,2] <- 0
+x[81, 2] <- 1 # contrast subject image against everyone else 
+                # 81 correpsonds to the the subject at row 81
                 # in the `sub_ucsd` data frame
-se <- t(as.matrix( mrss * c %*% XX %*% c)) # standard errors
+fit <- .lm.fit(x, imat)
+mrss <- sqrt(colSums(fit$residuals) / (n - 1)) # mean residual sum of squares
+c <- matrix(c(1, -1), nrow =1 ) # contrast
+
+  qr <-z[c("qr", "qraux", "pivot", "tol", "rank")]
+  qr <-structure(qr, class = "qr")
+  XX <-chol2inv(qr.R(qr))
+
+se <- t(as.matrix( mrss * (c[1,] %*% XX %*% c[1,]))) # standard errors
 se[se == 0] <- 1 # 0/0 produces NAs, so now all 0s are divided by 1
 statmat <- (c %*% fit$coefficients) / se # create t-statistic
 istat <- makeImage(mask, statmat)
-
+antsImageWrite(istat, file = paste(dirname(paths[81]), "/scaled.nii.gz", sep = ""))
 
 # alternatively, we can loop over the entire dataset
 x <- matrix(nrow = n, ncol = 2)
 x[, 1] <- 1
-x[,2] <- 0
+x[, 2] <- 0
 new_ilist <- list()
 c <- matrix(c(1, -1), nrow =1 ) # contrast
+
+progress <- txtProgressBar(min = 0, max = n, style = 3)
 for ( i in 1:n){
   if (i > 1)
     x[i - 1, 2] <- 0
   x[i, 2] <- 1
   fit <- .lm.fit(x, imat)
   mrss <- sqrt(fit$residuals / (n - 1))
-  se <- t(as.matrix( mrss * c %*% XX %*% c))
+  se <- t(as.matrix( mrss * c[1,] %*% XX %*% c[1,]))
   se[se == 0] <- 1
   statmat <- (c %*% fit$coefficients) / se
-  img <- makeImage(mask, statmat)
-  renderSurfaceFunction
+  istat <- makeImage(mask, statmat)
+  antsImageWrite(istat, file = paste(dirname(paths[i]), "/scaled.nii.gz", sep = ""))
+  setTxtProgressBar(progress, i)
 }
+close(progress)
